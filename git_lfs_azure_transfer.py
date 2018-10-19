72e6a82128be3afbbbb4a9da5d543c9125363f5a
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2018-10-19 13:21:51 +0200

    more scaffolding

--- git_lfs_azure_transfer.py
+++ git_lfs_azure_transfer.py
@@ -1,3 +1,38 @@
+import json
+import sys
+
+from azure.storage.blob import BlockBlobService
+
+
+def read():
+    request = json.loads(sys.stdin.readline().strip())
+    if request['event'] == 'terminate':
+        sys.exit()
+    return request
+
+
+def write(response):
+    sys.stdout.write(json.dumps(response) + '\n')
+    sys.stdout.flush()
+
+
+def download(init):
+    pass
+
+
+def upload(init):
+    pass
+
+
+def main():
+    init = read()
+    if init['event'] == 'init':
+        respond({})
+        if init['operation'] == 'download':
+            download(init)
+        elif init['operation'] == 'upload':
+            upload(init)
+
 
 if __name__ == "__main__":
-    print("hi")
+    main()
