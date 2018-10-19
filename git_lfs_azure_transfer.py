8d8a54591467735de697e905dbeee061c8d92242
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2018-10-19 14:06:43 +0200

    basic implementation

--- git_lfs_azure_transfer.py
+++ git_lfs_azure_transfer.py
@@ -1,37 +1,85 @@
+from functools import lru_cache
 import json
-import sys
+from sys import stdin, stdout
+import tempfile
+from urllib.parse import urlparse
 
 from azure.storage.blob import BlockBlobService
 
 
 def read():
-    request = json.loads(sys.stdin.readline().strip())
+    request = json.loads(stdin.readline().strip())
     if request['event'] == 'terminate':
-        sys.exit()
+        return
     return request
 
 
 def write(response):
-    sys.stdout.write(json.dumps(response) + '\n')
-    sys.stdout.flush()
+    stdout.write(json.dumps(response) + '\n')
+    stdout.flush()
 
 
-def download(init):
-    pass
+@lru_cache(maxsize=None)
+def block_blob_service(account_name, sas_token):
+    return BlockBlobService(account_name=account_name, sas_token=sas_token)
 
 
-def upload(init):
-    pass
+def parse_href(href):
+    # href: f"https://{account_name}.blob.core.windows.net/{self.container}/{blob}?{sas_query}"
+    url = urlparse(href)
+    account_name = url.hostname.split('.')[0]
+    _, container_name, blob_name = url.path.split('/')
+    sas_token = url.query
+    return account_name, container_name, blob_name, sas_token
+
+
+def report_progress(oid, bytes_so_far, bytes_since_last):
+    write({
+        'event': 'progress',
+        'oid': oid,
+        'bytesSoFar': bytes_so_far,
+        'bytesSinceLast': bytes_since_last,
+    })
+
+
+def temp_file_path():
+    fd, path = tempfile.mkstemp()
+    os.close(fd)
+    return path
+
+
+def handle_transfers(operation):
+    transfer = read()
+    while transfer:
+        oid, href = transfer['oid'], transfer['action']['href']
+        account_name, container_name, blob_name, sas_token = parse_href(href)
+        service = block_blob_service(account_name, sas_token)
+
+        last_current = 0
+        def progress_callback(current, total):
+            report_progress(oid, current, current - last_current)
+            last_current = current
+
+        if operation == 'upload':
+            path = transfer['path']
+            service.create_blob_from_path(container_name, blob_name, path, progress_callback=progress_callback)
+        elif operation == 'download':
+            path = temp_file_path()
+            service.get_blob_to_path(container_name, blob_name, path, progress_callback=progress_callback)
+
+        complete_payload = {'event': 'complete', 'oid': oid}
+        if operation == 'download':
+            complete_payload['path'] = path
+        write(complete_payload)
+
+        transfer = read()
 
 
 def main():
     init = read()
-    if init['event'] == 'init':
-        respond({})
-        if init['operation'] == 'download':
-            download(init)
-        elif init['operation'] == 'upload':
-            upload(init)
+    assert init['event'] == 'init'
+    respond({})
+    handle_transfers(init['operation'])
 
 
 if __name__ == "__main__":
